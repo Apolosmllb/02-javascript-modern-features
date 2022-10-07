7b12970b73d4596e600aee089175dec1e3f97237
Apolos <minermllb@gmail.com>
2022-10-07 00:51:45 -0500

    add new features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Variables y Constantes
+const nombre = 'miner';
+const apellido = 'Lozano';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let valorDado = 5;
+valorDado = 4;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(nombre, apellido, valorDado)
